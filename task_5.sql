bd6ff1fc1c536b24499d4705de4daa401ec4d1b8
kim-mkuu <kimukuphilip10@gmail.com>
2025-07-05 14:25:50 +0300

    adding sql script that inserts a single row in the table customer.

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- Insert a single row into Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (
+        1,
+        'Cole Baidoo',
+        'cbaidoo@sandtech.com',
+        '123 Happiness Ave.'
+    );
